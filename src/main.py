3d74e8a4ebec55c24a5021106d7afb2d2102e62e
KG Developer <dev@kg-project.local>
2026-04-15 11:42:17 +0800

    feat(phase3): add query and config API routes

--- src/main.py
+++ src/main.py
@@ -3,9 +3,9 @@ from src.api.routes import router
 from src.api.middleware import logging_middleware
 from src.api.admin_routes import router as admin_router
 from src.api.graph_routes import router as graph_router
+from src.api.query_routes import router as query_router
+from src.api.config_routes import router as config_router
 from src.logs import logger
-
-logger = logging.getLogger(__name__)
 
 app = FastAPI(title='动力电池拆卸知识图谱推理系统', version='1.0.0')
 
@@ -14,6 +14,8 @@ app.middleware('http')(logging_middleware)
 app.include_router(router)
 app.include_router(admin_router, prefix='/admin')
 app.include_router(graph_router, prefix='/api/v1')
+app.include_router(query_router, prefix='/api/v1')
+app.include_router(config_router, prefix='/api/v1')
 
 
 @app.on_event('shutdown')
