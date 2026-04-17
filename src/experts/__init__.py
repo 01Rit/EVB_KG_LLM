83303711a90b65dd2193a9dedbb0e8fa4d5b5b50
KG Developer <dev@kg-project.local>
2026-04-17 15:29:47 +0800

    feat: add SafetyExpert for safety factor scoring

--- src/experts/__init__.py
+++ src/experts/__init__.py
@@ -1,3 +1,6 @@
 from src.experts.base_expert import BaseExpert
+from src.experts.safety_expert import SafetyExpert
+from src.experts.production_expert import ProductionExpert
+from src.experts.quality_expert import QualityExpert
 
-__all__ = ['BaseExpert']
+__all__ = ['BaseExpert', 'SafetyExpert', 'ProductionExpert', 'QualityExpert']
